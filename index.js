98317f17bf9af174211d238c2725c218d4e56586
user <zhangzhenxi0316@163.com>
2021-03-21 04:18:04 +0800

    jwt

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var express = require('express')
 let login = require('./router/login')
 let register = require('./router/register')
 let goods = require('./router/goods')
+let cookieParser = require('cookie-parser')
 var app = express()
-
 // post 
 const bodyParser = require('body-parser');
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods","*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+app.use(cookieParser())
+app.use(bodyParser.json())
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/login', login)
--- router/goods.js
+++ router/goods.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var query = require("../mysql/root");
-
+let jwt = require("jsonwebtoken");
+let secrect = "qwert";
 router.get("/get_all", async function (req, res) {
   let result = null;
   try {
@@ -19,19 +20,13 @@ router.get("/get_goods_by_category", async (req, res) => {
   let result = null;
   try {
     result = await query(sql, category);
-    // console.log(result)
-  } catch (error) {
-    console.log(error);
-    res.send({ status: "error", msg: "查询失败" });
-  }
-  res.send({ status: "success", msg: "查询成功", data: result });
-});
-router.get("/get_goods_by_id", async (req, res) => {
-  const { id } = req.query;
-  let sql = "select * from goods_info where id=?";
-  let result = null;
-  try {
-    result = await query(sql, id);
+    if (result.length === 0) {
+      res.send({
+        status: "error",
+        msg: "没有这个分类",
+      });
+      return;
+    }
     // console.log(result)
   } catch (error) {
     console.log(error);
@@ -40,8 +35,45 @@ router.get("/get_goods_by_id", async (req, res) => {
   res.send({ status: "success", msg: "查询成功", data: result });
 });
 
-router.get("/get_collect_by_id", async (req, res) => {
+//通过商品id查询商品信息
+router.get("/get_goods_by_id", async (req, res) => {
+    let user_id = req.cookies.jwt && jwt.verify(req.cookies.jwt,secrect).user_id
   const { id } = req.query;
+  let sql = "select * from goods_info where id=?";
+  let sql1 = "select * from shopping_car where user_id =? and goods_id=?"
+  let result = null;
+  try {
+    result = await query(sql, id);
+    if(user_id){
+        let res = await query(sql1,[user_id,id])
+        res.length===0?result[0].isCollect=false:result[0].isCollect = true
+    }
+    if (result.length === 0) {
+      res.send({
+        status: "error",
+        msg: "商品已经被删除",
+      });
+      return;
+    }
+    // console.log(result)
+  } catch (error) {
+    console.log(error);
+    res.send({ status: "error", msg: "查询失败" });
+  }
+  res.send({ status: "success", msg: "查询成功", data: result[0] });
+});
+
+//通过用户id查询加入购物车的东西
+router.get("/get_collect_by_id", async (req, res) => {
+    const jwtString = req.cookies.jwt
+    let id ;
+    if(jwtString){
+       id = jwt.verify(jwtString,secrect).user_id
+    }else{
+        res.send({status:301,msg:'登陆失效，重新登陆'})
+        reuturn 
+    }
+//   const { id } = req.query;
   let sql = "select * from shopping_car where user_id = ?";
   let result = null;
   try {
@@ -57,6 +89,7 @@ router.get("/get_collect_by_id", async (req, res) => {
   }
   res.send({ status: "success", msg: "查询成功", data: result });
 });
+
 async function collect(gid, uid) {
   let sql = "insert into shopping_car (goods_id,user_id) values (?,?)";
   let result = await query(sql, [gid, uid]);
@@ -68,27 +101,33 @@ async function canelCollect(gid, uid) {
   return result;
 }
 
-
 router.post("/collect", async (req, res) => {
-  const { user_id, goods_id } = req.body;
+    const jwtString = req.cookies.jwt
+    let user_id ;
+    if(jwtString){
+        user_id = jwt.verify(jwtString,secrect).user_id
+    }else{
+        res.send({status:301,msg:'登陆失效，重新登陆'})
+        reuturn 
+    }
+  const {  goods_id } = req.body;
   //查询购物车里有没有添加过
   let sql = "select * from shopping_car where user_id=? and goods_id =?";
   let result = null;
   try {
     result = await query(sql, [user_id, goods_id]);
     if (result.length) {
-        //添加过了取消收藏
+      //添加过了取消收藏
       result = canelCollect(goods_id, user_id);
-      res.send({ status: "success", msg: "取消收藏成功" });
+      res.send({ status: "success", msg: "取消收藏成功",data:false });
       return;
     }
     //没有添加过取消收藏
     result = collect(goods_id, user_id);
-    
   } catch (error) {
     console.log(error);
     res.send({ status: "error", msg: "收藏失败" });
   }
-  res.send({ status: "success", msg: "收藏成功" });
+  res.send({ status: "success", msg: "收藏成功",data:true });
 });
 module.exports = router;
