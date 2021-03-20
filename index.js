626acf6911610b5b161bc57dee16c63eaddcbba6
1084350607 <5550076+tsy210258@user.noreply.gitee.com>
2021-03-20 21:37:22 +0800

    添加登录和注册

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require('express')
-var login = require('./router/login')
-
+let login = require('./router/login')
+let register = require('./router/register')
 var app = express()
+
+// post 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.use('/login', login)
+app.use('/register', register)
+
 
 app.listen(3000)
--- mysql/root.js
+++ mysql/root.js
@@ -1,18 +1,28 @@
-var mysql = require('mysql');
-let config = require('./config')
-var connection = mysql.createConnection(config);
+const mysql = require('mysql');
+const config = require('./config')
 
-function query(sql) {
-    return new Promise((resolve, reject) => {
-        connection.connect(); // 建立链接
-        connection.query(sql, function (err, result) {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(result)
-            }
+let pool = mysql.createPool(config);
+// connection.connect();
+
+let query = function (sql, values) {
+  // 返回一个 Promise
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err);
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+          // 结束会话
+          connection.release();
         });
-    })
-}
+      }
+    });
+  });
+};
 
 module.exports = query
