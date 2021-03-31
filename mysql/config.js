7b45edb3c381b4bf6b8c69fb7caca74a2b21be1b
user <zhangzhenxi0316@163.com>
2021-03-31 09:48:27 +0800

    fix

--- mysql/config.js
+++ mysql/config.js
@@ -1,7 +1,7 @@
 const config = {
     host: 'localhost', // 连接的本地mysql
     user: 'root', // 用户名
-    password: '210258', // 密码
+    password: '031648', // 密码
     port: '3306', // 端口号
     database: 'walsmart' // 表名
 }
